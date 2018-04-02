1e93da000f25bf16f143a3c246f828a8a7d233d3
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-02 11:42:28 +0000

    display_flashヘルパーを追加する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def display_flash
+    flash.each do |type, msg|
+      concat content_tag(:div, msg, class: "alert alert-#{type}")
+    end
+    nil
+  end
 end
