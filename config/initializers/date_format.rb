fc34d7f1bffe05706e8e79fed1991a4130a73b1a
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-03 07:18:56 +0000

    date/timeフォーマットを指定する

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,6 @@
+Date::DATE_FORMATS[:default] = '%Y/%m/%d'
+Time::DATE_FORMATS[:default] = '%Y/%m/%d'
+
+# 短縮形式(例: 2014/02/10 => 14/02/10)
+Date::DATE_FORMATS[:short] = '%y/%m/%d'
+Time::DATE_FORMATS[:short] = '%y/%m/%d'
