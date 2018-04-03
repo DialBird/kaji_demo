b6045312155f03f5da49e5d277df52f86a204fd7
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-03 12:32:16 +0000

    user/home_controllerのRSpecを修正する

--- spec/controllers/user/home_controller_spec.rb
+++ spec/controllers/user/home_controller_spec.rb
@@ -3,10 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe User::HomeController, type: :controller do
+  let!(:user) { FactoryBot.create(:user) }
+
   describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'when user logged in' do
+      before { user_log_in user }
+
+      it 'render index' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+    context 'when user logged out' do
+      it 'redirect to login path' do
+        get :index
+        expect(response).to redirect_to user_login_path
+      end
     end
   end
 end
