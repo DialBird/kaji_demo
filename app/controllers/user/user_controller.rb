913ceb2412e8da29ee1ebf6ec4a9fdec322244f4
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-03 08:02:13 +0000

    User編集のロジックを追加する

--- app/controllers/user/user_controller.rb
+++ app/controllers/user/user_controller.rb
@@ -5,5 +5,18 @@ class User::UserController < User::ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    if current_user.update(user_params)
+      redirect_to user_account_path(@user), success: t(:update, scope: 'success.messages')
+    else
+      flash.now[:danger] = t(:update, scope: 'errors.messages')
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(User::PERMITTED_ATTRIBUTES)
+  end
 end
