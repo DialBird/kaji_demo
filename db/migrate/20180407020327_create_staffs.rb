0ecc623f544d3dccc34023fcf6a9ccb3f71e029a
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-07 02:10:37 +0000

    migrate staff model

---
+++ db/migrate/20180407020327_create_staffs.rb
@@ -0,0 +1,22 @@
+class CreateStaffs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :staffs, comment: '家事代行スタッフ' do |t|
+      t.integer :gender_id, null: false, comment: '性別(gender.yml参照)'
+      t.integer :age, null: false, comment: '年齢'
+      t.string :name, null: false, comment: '名前'
+      t.datetime :birthday, comment: '誕生日'
+      t.string :email, null: false, comment: 'メール'
+      t.string :phone, null: false, comment: '電話番号'
+      t.string :zip, comment: '郵便番号'
+      t.string :state, comment: '都道府県'
+      t.string :city, comment: '市区町村'
+      t.string :street, comment: '番地'
+      t.string :password_digest
+      t.string :remember_digest
+
+      t.timestamps
+    end
+    add_index :staffs, :name, unique: true
+    add_index :staffs, :email, unique: true
+  end
+end
