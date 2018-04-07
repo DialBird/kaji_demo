b645e054ddfd6172f670e40e7a6f3b4bdfadc5c0
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-07 02:47:17 +0000

    Add flow to Staff Login page

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-class Staff::SessionsController < ApplicationController
+class Staff::SessionsController < Staff::ApplicationController
+  skip_before_action :authenticate_staff?
+
   def new; end
 
   def create; end
