f03d24df120c1c8b44bf5d01473ac0b013338cc5
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-07 02:12:19 +0000

    bundle exec annotate

--- spec/factories/staffs.rb
+++ spec/factories/staffs.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: staffs # 家事代行スタッフ
+#
+#  id              :integer          not null, primary key
+#  gender_id       :integer          not null              # 性別(gender.yml参照)
+#  age             :integer          not null              # 年齢
+#  name            :string           not null              # 名前
+#  birthday        :datetime                               # 誕生日
+#  email           :string           not null              # メール
+#  phone           :string           not null              # 電話番号
+#  zip             :string                                 # 郵便番号
+#  state           :string                                 # 都道府県
+#  city            :string                                 # 市区町村
+#  street          :string                                 # 番地
+#  password_digest :string
+#  remember_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 FactoryBot.define do
   factory :staff do
     name "MyString"
