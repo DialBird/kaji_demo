6700834d350374b15fdb6207b2edfc87ae29d9ea
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-08 12:26:53 +0000

    Add flow to Operator Login page

--- app/controllers/operator/sessions_controller.rb
+++ app/controllers/operator/sessions_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class Operator::SessionsController < ApplicationController
+class Operator::SessionsController < Operator::ApplicationController
   def new; end
 end
--- spec/controllers/operator/sessions_controller_spec.rb
+++ spec/controllers/operator/sessions_controller_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Operator::SessionsController, type: :controller do
   describe 'GET #new' do
     it 'returns http success' do
+      http_login
       get :new
       expect(response).to have_http_status(:success)
     end
