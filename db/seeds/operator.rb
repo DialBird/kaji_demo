f990517b4cd25d1b56ca9043214de353b3b4b05d
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-08 12:30:32 +0000

    Add Operator db seed

---
+++ db/seeds/operator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Operator.create(is_admin: true,
+                name: 'TestOperator',
+                email: 'c@co.jp',
+                phone: '080-1234-5678',
+                password: '1234Abcd',
+                password_confirmation: '1234Abcd')
