b297c1286c1056c75814730d51889ec86ee7ad4e
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-08 11:36:01 +0000

    bundle exec annotate

--- app/models/operator.rb
+++ app/models/operator.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: operators # オペレーター
+#
+#  id              :integer          not null, primary key
+#  is_admin        :boolean          default(FALSE), not null # 管理者権限
+#  name            :string           not null                 # 名前
+#  email           :string           not null                 # メール
+#  phone           :string           not null                 # 電話番号
+#  password_digest :string
+#  remember_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Operator < ApplicationRecord
 end
--- spec/factories/operators.rb
+++ spec/factories/operators.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: operators # オペレーター
+#
+#  id              :integer          not null, primary key
+#  is_admin        :boolean          default(FALSE), not null # 管理者権限
+#  name            :string           not null                 # 名前
+#  email           :string           not null                 # メール
+#  phone           :string           not null                 # 電話番号
+#  password_digest :string
+#  remember_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 FactoryBot.define do
   factory :operator do
-    name "MyString"
+    name 'MyString'
   end
 end
