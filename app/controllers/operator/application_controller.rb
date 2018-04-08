1c25fb3a06fa272b91220d0aafc10f46490a46b9
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-08 12:07:58 +0000

    Add Basic Auth to Operator Page

--- app/controllers/operator/application_controller.rb
+++ app/controllers/operator/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Operator::ApplicationController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_USERNAME'], password: ENV['BASIC_PASSWORD']
   layout 'operator_layout'
 end
