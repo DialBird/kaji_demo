c75efff5d0637e5a6fd22fd2d8dddd152172f88b
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-08 12:32:20 +0000

    Add Operator::SessionController details

--- app/controllers/operator/sessions_controller.rb
+++ app/controllers/operator/sessions_controller.rb
@@ -2,4 +2,20 @@
 
 class Operator::SessionsController < Operator::ApplicationController
   def new; end
+
+  def create
+    operator = Operator.find_by(email: params[:session][:email].downcase)
+    if operator&.authenticate(params[:session][:password])
+      operator_log_in operator
+      redirect_to operator_root_path, success: t(:login, scope: 'success.messages')
+    else
+      flash.now[:danger] = t(:login, scope: 'errors.messages')
+      render 'new'
+    end
+  end
+
+  def destroy
+    operator_log_out
+    redirect_to root_path, success: t(:logout, scope: 'success.messages')
+  end
 end
