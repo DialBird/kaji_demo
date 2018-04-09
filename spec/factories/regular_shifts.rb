f62cd6d848e50405d07411fbc4035f27241e706e
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-09 10:16:44 +0000

    Exec Annotate

--- spec/factories/regular_shifts.rb
+++ spec/factories/regular_shifts.rb
@@ -1,5 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: regular_shifts # 通常時のシフト
+#
+#  id         :integer          not null, primary key
+#  staff_id   :integer          not null              # スタッフID
+#  mon_start  :string           not null              # 月曜日開始
+#  mon_end    :string           not null              # 月曜日終了
+#  tue_start  :string           not null              # 火曜日開始
+#  tue_end    :string           not null              # 火曜日終了
+#  wed_start  :string           not null              # 水曜日開始
+#  wed_end    :string           not null              # 水曜日終了
+#  thu_start  :string           not null              # 木曜日開始
+#  thu_end    :string           not null              # 木曜日終了
+#  fri_start  :string           not null              # 金曜日開始
+#  fri_end    :string           not null              # 金曜日終了
+#  sat_start  :string           not null              # 土曜日開始
+#  sat_end    :string           not null              # 土曜日終了
+#  sun_start  :string           not null              # 日曜日開始
+#  sun_end    :string           not null              # 日曜日終了
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :regular_shift do
-    mon_start "MyString"
+    mon_start 'MyString'
   end
 end
