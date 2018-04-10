5ffdee8522505546de70e421669173215cf9f240
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-10 10:58:07 +0000

    Exec Annotate

--- app/models/irregular_off.rb
+++ app/models/irregular_off.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: irregular_offs # 急な休み
+#
+#  id         :integer          not null, primary key
+#  staff_id   :integer          not null              # スタッフID
+#  date       :datetime         not null              # 日付
+#  start      :string           not null              # 開始時間
+#  end        :string           not null              # 終了時間
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class IrregularOff < ApplicationRecord
 end
--- spec/models/irregular_off_spec.rb
+++ spec/models/irregular_off_spec.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: irregular_offs # 急な休み
+#
+#  id         :integer          not null, primary key
+#  staff_id   :integer          not null              # スタッフID
+#  date       :datetime         not null              # 日付
+#  start      :string           not null              # 開始時間
+#  end        :string           not null              # 終了時間
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe IrregularOff, type: :model do
