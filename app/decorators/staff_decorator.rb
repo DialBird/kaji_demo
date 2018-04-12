3108b9b402b611bc8f69f4e016c3a9fd5a6daebb
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-12 07:47:11 +0000

    Add regular_shifts_json Decorator to Staff

--- app/decorators/staff_decorator.rb
+++ app/decorators/staff_decorator.rb
@@ -3,6 +3,13 @@
 class StaffDecorator < Draper::Decorator
   delegate_all
 
+  def regular_shifts_json
+    # shifts should be listed from Sunday to Saturday
+    regular_shifts
+      .order(:dayofweek_id)
+      .to_json(only: %i[dayofweek start end].freeze)
+  end
+
   def irregular_offs_json
     irregular_offs.to_json(only: %i[date start end].freeze)
   end
