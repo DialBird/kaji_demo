2bed792a7c897e4ae88b3a64f29cf1b2def40df5
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-12 07:59:25 +0000

    Exec Annotate

--- app/models/regular_shift.rb
+++ app/models/regular_shift.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: regular_shifts # 通常時のシフト
 #
 #  id           :integer          not null, primary key
-#  staff_id     :integer          not null                   # スタッフID
+#  staff_id     :integer          not null              # スタッフID
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
-#  dayofweek_id :integer          default(0), not null       # 曜日（day_of_week.yml参照）
-#  start        :string           default("00:00"), not null # 開始時刻（HH:MM）
-#  end          :string           default("00:00"), not null # 終了時刻（HH:MM）
+#  dayofweek_id :integer          default(0), not null  # 曜日（day_of_week.yml参照）
+#  start_at     :integer          default(0), not null  # 開始時刻（time_block.yml参照）
+#  end_at       :integer          default(0), not null  # 開始時刻（time_block.yml参照）
 #
 
 class RegularShift < ApplicationRecord
