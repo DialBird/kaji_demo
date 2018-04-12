6e85c5c5ccf311416b8883de3afb7fd91bb67244
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-12 02:26:47 +0000

    Exec Annotate

--- spec/factories/regular_shifts.rb
+++ spec/factories/regular_shifts.rb
@@ -4,24 +4,13 @@
 #
 # Table name: regular_shifts # 通常時のシフト
 #
-#  id         :integer          not null, primary key
-#  staff_id   :integer          not null              # スタッフID
-#  mon_start  :string           not null              # 月曜日開始
-#  mon_end    :string           not null              # 月曜日終了
-#  tue_start  :string           not null              # 火曜日開始
-#  tue_end    :string           not null              # 火曜日終了
-#  wed_start  :string           not null              # 水曜日開始
-#  wed_end    :string           not null              # 水曜日終了
-#  thu_start  :string           not null              # 木曜日開始
-#  thu_end    :string           not null              # 木曜日終了
-#  fri_start  :string           not null              # 金曜日開始
-#  fri_end    :string           not null              # 金曜日終了
-#  sat_start  :string           not null              # 土曜日開始
-#  sat_end    :string           not null              # 土曜日終了
-#  sun_start  :string           not null              # 日曜日開始
-#  sun_end    :string           not null              # 日曜日終了
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :integer          not null, primary key
+#  staff_id     :integer          not null                   # スタッフID
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  dayofweek_id :integer          default(0), not null       # 曜日（day_of_week.yml参照）
+#  start        :string           default("00:00"), not null # 開始時刻（HH:MM）
+#  end          :string           default("00:00"), not null # 終了時刻（HH:MM）
 #
 
 FactoryBot.define do
