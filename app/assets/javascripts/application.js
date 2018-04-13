b8b5856c6d45177c6c8da21c1a1d20bf04a6c45f
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-13 12:21:27 +0000

    Uninstall turbolink

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require rails-ujs
-//= require turbolinks
 //= require bootstrap-datepicker
 //= require lib/moment.min
 //= require lib/fullcalendar
