dea8fbcc98e42b584e2bf474865969192c196f20
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-15 04:43:15 +0000

    Add OrderStatusLabel Helper

--- app/helpers/label_helper.rb
+++ app/helpers/label_helper.rb
@@ -1,20 +1,39 @@
 # frozen_string_literal: true
 
 module LabelHelper
+  def render_label(type)
+    content_tag :span, class: "label label-#{type}" do
+      yield
+    end
+  end
+
   def gender_label(gender)
     case gender
     when Gender::MAN
-      content_tag :span, class: 'label label-primary' do
-        concat content_tag :i, '', class: 'fa fa-mars'
+      render_label('primary') do
+        fa_icon 'mars'
       end
     when Gender::WOMAN
-      content_tag :span, class: 'label label-danger' do
-        concat content_tag :i, '', class: 'fa fa-venus'
+      render_label('danger') do
+        fa_icon 'venus'
       end
     when Gender::OTHER
-      content_tag :span, class: 'label label-success' do
-        concat content_tag :i, '', class: 'fa fa-transgender'
+      render_label('success') do
+        fa_icon 'transgender'
       end
+    end
+  end
+
+  def order_status_label(order)
+    case order.order_status
+    when OrderStatus::CHECKING
+      render_label('default') { 'checking' }
+    when OrderStatus::ACCEPTED
+      render_label('warning') { 'accepted' }
+    when OrderStatus::COMPLETED
+      render_label('success') { 'completed' }
+    when OrderStatus::CANCELED
+      render_label('danger') { 'canceled' }
     end
   end
 end
