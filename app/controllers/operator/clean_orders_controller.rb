26aa04147a7cdc9cb26f1c08d4a3bcc58fe5ab61
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-16 09:48:34 +0000

    Add operator/clean_order#index

--- app/controllers/operator/clean_orders_controller.rb
+++ app/controllers/operator/clean_orders_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Operator::CleanOrdersController < Operator::ApplicationController
-  def index; end
+  def index
+    @clean_orders = CleanOrder.all.order(:created_at)
+  end
 end
