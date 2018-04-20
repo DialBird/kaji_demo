3360845374f6b6d4eb03944f0ad39c4f45cb313a
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-20 12:25:17 +0000

    Add Vaildation about spots to CleanOrder

--- app/models/clean_order.rb
+++ app/models/clean_order.rb
@@ -40,6 +40,9 @@ class CleanOrder < ApplicationRecord
   validates :order_status_id, inclusion: { in: OrderStatus.all.map(&:id) }
   validate :valid_times?
 
+  # TODO: Need a context?
+  validates :spots, presence: true
+
   OrderStatus.all.pluck(:type).each do |status|
     define_method "#{status}?" do
       order_status == OrderStatus.find_by(type: status)
