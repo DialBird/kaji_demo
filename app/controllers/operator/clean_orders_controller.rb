a19d1b2fc6f6f9c5e09127a0a6066cd292947e69
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-20 12:47:16 +0000

    Add Operator/CleanOrder View

--- app/controllers/operator/clean_orders_controller.rb
+++ app/controllers/operator/clean_orders_controller.rb
@@ -4,4 +4,8 @@ class Operator::CleanOrdersController < Operator::ApplicationController
   def index
     @clean_orders = CleanOrder.all.order(:created_at)
   end
+
+  def show
+    @clean_order = CleanOrder.find(params[:id]).decorate
+  end
 end
