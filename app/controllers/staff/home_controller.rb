c02a2f749f8edc1726a35b1f4bc4d508855e4c97
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-21 02:25:23 +0000

    Edit Staff Home View to show order list

--- app/controllers/staff/home_controller.rb
+++ app/controllers/staff/home_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Staff::HomeController < Staff::ApplicationController
-  def index; end
+  def index
+    @clean_orders = CleanOrder
+                      .where(staff_id: current_staff.id)
+                      .order(:created_at)
+  end
 end
