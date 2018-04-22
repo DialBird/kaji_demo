0da24bc5fb1f4e21c6da1ca3d90e8f18f3c16ee7
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-22 00:26:45 +0000

    Exec Annotate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer          not null              # ユーザーID
+#  clean_order_id :integer          not null              # 清掃オーダーID
+#  date           :date                                   # コメント日付
+#  star           :integer          default(0), not null  # スター
+#  comment        :text                                   # コメント
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Review < ApplicationRecord
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,3 +1,19 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer          not null              # ユーザーID
+#  clean_order_id :integer          not null              # 清掃オーダーID
+#  date           :date                                   # コメント日付
+#  star           :integer          default(0), not null  # スター
+#  comment        :text                                   # コメント
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
